6d2e132d0e2c674d4f0f6fcdfd5d74878986f9a7
Timileyin Oladejo <timileyinsamuel025@gmail.com>
2025-02-28 15:46:26 +0100

    Hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+
+	printf("The size of float is %f\n", sizeof(float));
+	printf("The size of character is %c\n", sizeof(char));
+	printf("The size of integer is %i\n", sizeof(int));
+	printf("The size of double is %lu", sizeof(double));
+	
+
+return 0;
+}
