5aef78c163edf3e9af389a984935eeaa1309ab41
Timileyin Oladejo <timileyinsamuel025@gmail.com>
2025-03-09 18:25:21 +0100

    New Commit

---
+++ allow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+int main(){
+	
+	int new_num=65;
+
+	do {
+	printf("Print %d = %c\n, new_num, new_num);
+
+	new_num++;
+	}
+
+	while(new_num < 72);
+
+	printf("Escape\n");
+
+return 0;
+}
