a33b3a79e614692537da5797b856498698ab1baa
Timileyin Oladejo <timileyinsamuel025@gmail.com>
2025-03-19 03:07:57 +0100

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,6 @@
 
 int main(void)
 {
-    int a, b, c;
-
-    printf("Enter a new number: %d", a);
-    scanf("%d", &a)
-
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
