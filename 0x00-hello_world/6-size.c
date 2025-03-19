d8ed18a182452e9ecae2e8e2a84cb7be1e774ab5
Timileyin Oladejo <timileyinsamuel025@gmail.com>
2025-03-19 03:03:51 +0100

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,10 @@ int main(){
 	printf("The size of character is %lu\n", sizeof(char));
 	printf("The size of integer is %lu\n", sizeof(int));
 	printf("The size of double is %lu", sizeof(double));
+	printf("The size of long int is %lu\n", sizeof(long int));
+	printf("The size of long long int is %lu\n", sizeof(long long int));
 	
 
 return 0;
 }
+
